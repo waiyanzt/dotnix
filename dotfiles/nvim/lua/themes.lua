e27b289af805487fab48d6e936e21f46974c477e
Marcus Tin <marcus.tinw@gmail.com>
2025-11-18 23:03:03 -0800

    nvim updoot

--- dotfiles/nvim/lua/themes.lua
+++ dotfiles/nvim/lua/themes.lua
@@ -6,40 +6,46 @@ return {
     opts = {},
   },
 
-  -- Zenbones
+  -- Zenbones (Commented out as in your original)
   -- {
   --   "mcchrish/zenbones.nvim",
   --   dependencies = { "rktjmp/lush.nvim" },
   --   priority=1000,
   --   config = function()
   --     vim.g.zenbones_darken_comments = 50
-  --     vim.g.zenbones_dark_mode = true -- Dark version
+  --     vim.g.zenbones_dark_mode = true
   --     vim.cmd.colorscheme("kanagawabones")
   --   end,
   -- },
 
-  -- Eva-01
-  -- {
-  --   "hachy/eva01.vim",
-  --   opts = {},
-  --     -- or
-  --     -- vim.cmd.colorscheme "eva01-LCL"
-  
-	-- Gruvbox
+  -- Gruvbox (Disabled auto-load so it doesn't conflict)
+  {
+      "ellisonleao/gruvbox.nvim",
+      -- priority = 1000, -- Lowered priority/commented out
+      -- config = function()
+      --     vim.cmd.colorscheme("gruvbox")
+      -- end,
+  },
 
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("gruvbox")
-		end,
-	},
   -- Catppuccin
   {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
-      flavour = "mocha", -- mocha is the dark variant
+      flavour = "mocha",
     },
+  },
+
+  -- BlackMetal
+  {
+    "metalelf0/black-metal-theme-neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("black-metal").setup({
+        theme = "darkthrone",
+      })
+      require("black-metal").load()
+    end,
   },
 }
