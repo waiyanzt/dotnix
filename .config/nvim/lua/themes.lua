7235d6784b6ebc021aaae5d482acf7075e2f08a8
Marcus Tin <marcus.tinw@gmail.com>
2025-12-06 05:24:14 -0800

    changes

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -1,3 +1,15 @@
+require('lazy').setup {
+  {
+    "hachy/eva01.vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "eva01"
+      -- or
+      -- vim.cmd.colorscheme "eva01-LCL"
+    end,
+  }
+}
 -- return {
 --   -- Rose Pine
 --   {
