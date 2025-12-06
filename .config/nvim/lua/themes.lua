0f421cbb8ff0ec1cd1bad5c511938966bbff59bf
Marcus Tin <marcus.tinw@gmail.com>
2025-12-06 05:45:20 -0800

    melange.nvim

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -52,14 +52,15 @@ return {
   --     require("black-metal").load()
   --   end,
   -- },
-  {
-    "hachy/eva01.vim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "eva01"
-      -- or
-      -- vim.cmd.colorscheme "eva01-LCL"
-    end,
-  }
+  -- {
+  --   "hachy/eva01.vim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme "eva01"
+  --     -- or
+  --     -- vim.cmd.colorscheme "eva01-LCL"
+  --   end,
+  -- }
+   { "savq/melange-nvim" }
 }
